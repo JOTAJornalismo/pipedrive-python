65ad316fe45896a87b46e8caa6f6ac5a8a18012e
Lucas Facchini <lucas.cf@live.com>
2022-11-04 11:48:50 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='pipedrive-python-lib',
-      version='1.1.5',
+      version='1.1.6',
       description='API wrapper for Pipedrive written in Python',
       long_description=read('README.md'),
       long_description_content_type="text/markdown",
